7cdac30b3eb9ffde5cbfc411e3eac7924336c702
kerblammo <31718829+kerblammo@users.noreply.github.com>
2018-02-15 11:57:32 -0400

    IceCream and Cake objects
    Created objects for IceCream and Cake which have attributes to evaluate recipes. After consideration, Fingers object was unnecessary as it does not need to be customized.
    
    TODO:
    * Create customization window
    * Allow customization to modify recipes
    * Give customized item to player

---
+++ CookThulhu/CookThulhu/Cake.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Cake is an object that represents a cake the player may serve to a customer
+/// </summary>
+namespace CookThulhu
+{
+    class Cake
+    {
+        public int ItemID { get; set; }
+        public bool Sigil { get; set; }
+        public bool Skulls { get; set; }
+        public bool Cherries { get; set; }
+
+        /// <summary>
+        /// Construct a cake with initial values
+        /// </summary>
+        public Cake()
+        {
+            ItemID = (int)MyEnums.ItemIDs.Cake;
+            Sigil = false;
+            Skulls = false;
+            Cherries = false;
+        }
+
+        /// <summary>
+        /// Construct a cake with specific values
+        /// </summary>
+        /// <param name="sigil">If the cake has a sigil</param>
+        /// <param name="skulls">If the cake has skulls</param>
+        /// <param name="cherries">If the cake has cherries</param>
+        public Cake(bool sigil, bool skulls, bool cherries)
+        {
+            ItemID = (int)MyEnums.ItemIDs.Cake;
+            Sigil = sigil;
+            Skulls = skulls;
+            Cherries = cherries;
+        }
+
+        
+    }
+}
---
+++ CookThulhu/CookThulhu/IceCream.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// IceCream is an object representing an ice cream that the player may serve to a patron
+/// </summary>
+namespace CookThulhu
+{
+    class IceCream
+    {
+        public int ItemID { get; set; }
+        public int ScoopsHazel { get; set; }
+        public int ScoopsMint { get; set; }
+        public int ScoopsStrawberry { get; set; }
+
+        /// <summary>
+        /// Construct an empty ice cream cone
+        /// </summary>
+        public IceCream()
+        {
+            ItemID = (int)MyEnums.ItemIDs.IceCream;
+            ScoopsHazel = 0;
+            ScoopsMint = 0;
+            ScoopsStrawberry = 0;
+        }
+
+        /// <summary>
+        /// Construct an ice cream cone with defined number of scoops
+        /// </summary>
+        /// <param name="hazel">Number of hazel scoops</param>
+        /// <param name="mint">Number of mint scoops</param>
+        /// <param name="strawberry">Number of strawberry scoops</param>
+        public IceCream(int hazel, int mint, int strawberry)
+        {
+            ItemID = (int)MyEnums.ItemIDs.IceCream;
+            ScoopsHazel = hazel;
+            ScoopsMint = mint;
+            ScoopsStrawberry = strawberry;
+        }
+    }
+}
